8b31272fc7f4a7929cb88017c8d71d0b15a5dd2a
ghodkevrushabh <ghodkevrushabh06@gmail.com>
2026-08-08 23:27:09 +0000

    Add CloudOptima generated IaC

---
+++ terraform/demo-payment-api/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "cloudoptima-tf-state-12345"
+    key    = "environments/demo-payment-api/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
