5c03f553e1427b027c9c49b0ed3f66345476ca94
Vrushabh Ghodke <ghodkevrushabh06@gmail.com>
2026-08-10 11:58:23 +0530

    Update Ubuntu AMI parameter to use gp2

--- terraform/test-app/main.tf
+++ terraform/test-app/main.tf
@@ -10,7 +10,7 @@ data "aws_subnet" "application" {
 }
 
 data "aws_ssm_parameter" "ubuntu_ami" {
-  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
 resource "aws_security_group" "app_sg" {
