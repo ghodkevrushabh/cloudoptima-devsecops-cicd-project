77b628aad5d82b0217bc1ddf59ef6ed897c1efe4
ghodkevrushabh <ghodkevrushabh06@gmail.com>
2026-08-15 10:51:39 +0000

    Store platform Terraform state in S3

--- terraform/platform/versions.tf
+++ terraform/platform/versions.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "cloudoptima-idp-artifacts-411902101270"
+    key    = "platform/terraform.tfstate"
+    region = "eu-north-1"
+  }
 }
 
 provider "aws" {
